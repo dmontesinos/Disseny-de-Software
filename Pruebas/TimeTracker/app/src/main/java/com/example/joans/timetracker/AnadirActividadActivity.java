46920c83a8c4d19e3e87dfe7f8bce4aa70838f8b
dmontesinos <55329263+dmontesinos@users.noreply.github.com>
2019-12-26 19:54:53 +0100

    Pantalla principal hecha
    Falta darle formato a las actividades

---
+++ Pruebas/TimeTracker/app/src/main/java/com/example/joans/timetracker/AnadirActividadActivity.java
@@ -0,0 +1,13 @@
+package com.example.joans.timetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnadirActividadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anadir_actividad);
+    }
+}
